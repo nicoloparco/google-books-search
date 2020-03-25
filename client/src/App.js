da2a3a6ecbbcab85da308eac82ff33bba9525bc7
Nico Loparco <nicoloparco75@gmail.com>
2020-03-24 23:40:11 -0400

    cleaned up files, moved to client, added bootsrtap to client

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="container border border-success">
+          <h1>Hello World</h1>
+        </div>
+    </div>
+  );
+}
+
+export default App;
