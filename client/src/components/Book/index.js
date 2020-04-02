8d2ffce7156528c79b2a44bfda357d85f0b68c8f
Nico Loparco <nicoloparco75@gmail.com>
2020-04-01 21:41:37 -0400

    passed props into Book component and completed books now fully map with all props in book search component

--- client/src/components/Book/index.js
+++ client/src/components/Book/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Book = (props) => (
     <div className="py-3">
-    <div className="container bg-light text-primary rounded-lg">
-        <h5 className="font-italic">{props.title}</h5>
+    <div className="container bg-light text-primary rounded-lg shadow-lg">
+        <h5 className="font-italic mt-3">{props.title}</h5>
         <p className="font-weight-bold">{props.author}</p>
         <div className="row py-3">
             <div className="col-md-4">
--- client/src/components/BookSearch/index.js
+++ client/src/components/BookSearch/index.js
@@ -25,10 +25,10 @@ const BookSearch = () => {
                 <input value={bookName} type="text" placeholder="Enter book name here" style={{width: "100%"}} onChange={(event) => setBookName(event.target.value)}></input>
                 <button className="btn btn-outline-primary bg-light mt-3" onClick={(event) => searchBook()}>Search</button>
                 
-                <div className="container mt-3 bg-primary text-light rounded-lg shadow-lg">
+                <div className="container mt-3 bg-primary text-light rounded-lg">
                 <h5 className="py-3">Results</h5>
                     {booksList.map((item, key) => (
-                        <Book key={key} title={item.volumeInfo.title} author={item.author} image={item.image} summary={item.summary} />
+                        <Book key={key} title={item.volumeInfo.title} author={item.volumeInfo.authors} image={item.volumeInfo.imageLinks ? item.volumeInfo.imageLinks.thumbnail : "No Image"} summary={item.volumeInfo.description ? item.volumeInfo.description.slice(0, 275) + "..." : "No summary available"} />
                     ))}
                 </div>
             </div>
